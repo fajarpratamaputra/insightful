e13d405d128014ff23153150a48162c3369b3e92
fajar <fajarpratamap@gmail.com>
2020-11-29 16:08:32 +0800

    notif

--- application/controllers/JsonConsultation.php
+++ application/controllers/JsonConsultation.php
@@ -52,6 +52,36 @@ class JsonConsultation extends CI_Controller {
 
 		$res = $this->m_consultation->add_consultation($data);
 
+		$token_deivce = $token_psikolog;
+		$token = '"'.$token_deivce.'"';
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+		CURLOPT_URL => "https://fcm.googleapis.com/fcm/send",
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => "",
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => "POST",
+		CURLOPT_POSTFIELDS =>'{
+			"registration_ids":['.$token.'],
+			"notification": {
+				"title":"insightful",
+				"body":"anda menerima pesan dari hrd"
+			}
+		  }',
+		CURLOPT_HTTPHEADER => array(
+			"Content-Type: application/json",
+			"Authorization: key=AAAATlTbhi4:APA91bFipNGjoz4HJp44Aj1JR6z9TFSnaw_TXZJIsmNnPeJls2K_4TdTd9qqf0HIyKrGt3Oaj70ovxoBpUtI2dsog77j7QQKebs2fGiQEi1dF74Hljfha5o4IHL2Xs3MaQw3vetMuIw0"
+		),
+		));
+
+		$response = curl_exec($curl);
+
+		curl_close($curl);
+
 		if(empty($res)) {
 			$this->output
 			->set_status_header(500)
--- application/views/psikolog/addPsikolog.php
+++ application/views/psikolog/addPsikolog.php
@@ -63,3 +63,138 @@
                 </form>
             </div>
         </div>
+
+        <script src="https://www.gstatic.com/firebasejs/5.10.1/firebase.js"></script>
+<script>
+    // Initialize Firebase
+    var firebaseConfig = {
+        apiKey: "AIzaSyA4dHoTfOlsUufN-lDvlBMYJRmY2N6lzow",
+        authDomain: "insightful-official.firebaseapp.com",
+        databaseURL: "https://insightful-official.firebaseio.com",
+        projectId: "insightful-official",
+        storageBucket: "insightful-official.appspot.com",
+        messagingSenderId: "336431121966",
+        appId: "1:336431121966:web:3496f51a4c7f73725a81bf",
+        measurementId: "G-931FEK227N"
+    };
+    firebase.initializeApp(firebaseConfig);
+
+    var database = firebase.database();
+
+    var lastIndex = 0;
+
+    // Get Data
+    firebase.database().ref('PsikologStatus/').on('value', function (snapshot) {
+        var value = snapshot.val();
+        var htmls = [];
+        $.each(value, function (index, value) {
+            if (value) {
+                htmls.push('<tr>\
+                <td>' + value.nis + '</td>\
+                <td>' + value.name + '</td>\
+                <td>' + value.age + '</td>\
+                <td><button data-toggle="modal" data-target="#update-modal" class="btn btn-info updateStudent" data-id="' + index + '">Update</button>\
+                <button data-toggle="modal" data-target="#remove-modal" class="btn btn-danger removeStudent" data-id="' + index + '">Delete</button></td>\
+            </tr>');
+            }
+            lastIndex = index;
+        });
+        $('#tbody').html(htmls);
+        $("#submitStudent").removeClass('disabled');
+    });
+
+    // Add Data
+    $('#submitStudent').on('click', function () {
+        var values = $("#addStudent").serializeArray();
+        var nis = values[0].value;
+        var name = values[1].value;
+        var age = values[2].value;
+        
+
+        //logic random
+        var result           = '';
+        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < 28; i++ ) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+
+        var userID = result;
+        firebase.database().ref('PsikologStatus/' + userID).set({
+            email: name,
+            login: 'false',
+            status: 1,
+        });
+
+        // Reassign lastID value
+        lastIndex = userID;
+        $("#addStudent input").val("");
+        // menampilkan alert
+        alert("Berhasil menambah data");
+    });
+
+    // Update Data
+    var updateID = 0;
+    $('body').on('click', '.updateStudent', function () {
+        updateID = $(this).attr('data-id');
+        firebase.database().ref('PsikologStatus/' + updateID).on('value', function (snapshot) {
+            var values = snapshot.val();
+            var updateData = '<div class="form-group">\
+                <label for="edit_nis" class="col-md-12 col-form-label">Nomor Induk Siswa</label>\
+                <div class="col-md-12">\
+                    <input id="edit_nis" type="text" class="form-control" name="edit_nis" value="' + values.nis + '" placeholder="Nomor Induk Siswa" required autofocus>\
+                </div>\
+            </div>\
+            <div class="form-group">\
+                <label for="edit_name" class="col-md-12 col-form-label">Nama Lengkap</label>\
+                <div class="col-md-12">\
+                    <input id="edit_name" type="text" class="form-control" name="edit_name" value="' + values.name + '" placeholder="Nama Siswa" required autofocus>\
+                </div>\
+            </div>\
+            <div class="form-group">\
+                <label for="edit_age" class="col-md-12 col-form-label">Usia</label>\
+                <div class="col-md-12">\
+                    <input id="edit_age" type="text" class="form-control" name="edit_age" value="' + values.age + '" placeholder="Usia" required autofocus>\
+                </div>\
+            </div>';
+
+            $('#updateBody').html(updateData);
+        });
+    });
+
+    $('.updateStudent').on('click', function () {
+        var values = $(".users-update-record-model").serializeArray();
+        var postData = {
+            nis: values[0].value,
+            name: values[1].value,
+            age: values[2].value,
+        };
+        var updates = {};
+        updates['/PsikologStatus/' + updateID] = postData;
+        firebase.database().ref().update(updates);
+        // menyembunyikan modal 
+        $("#update-modal").modal('hide');
+        // menampilkan alert
+        alert("Berhasil mengubah data");
+    });
+
+    // Remove Data
+    $("body").on('click', '.removeStudent', function () {
+        var id = $(this).attr('data-id');
+        $('body').find('.users-remove-record-model').append('<input name="id" type="hidden" value="' + id + '">');
+    });
+
+    $('.deleteStudent').on('click', function () {
+        var values = $(".users-remove-record-model").serializeArray();
+        var id = values[0].value;
+        firebase.database().ref('PsikologStatus/' + id).remove();
+        $('body').find('.users-remove-record-model').find("input").remove();
+        // menyembunyikan modal
+        $("#remove-modal").modal('hide');
+        // menampilkan alert
+        alert("Berhasil menghapus data");
+    });
+    $('.remove-data-from-delete-form').click(function () {
+        $('body').find('.users-remove-record-model').find("input").remove();
+    });
+</script>
