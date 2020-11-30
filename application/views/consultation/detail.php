997ca1a03e1b5f9e5729362a4b8436220f2f8c27
fajar <fajarpratamap@gmail.com>
2020-11-30 11:40:42 +0800

    api

--- application/views/consultation/detail.php
+++ application/views/consultation/detail.php
@@ -11,49 +11,35 @@
                 <!-- Row starts -->
                 <div class="row">
                     <!-- Column starts -->
-                <?php foreach($consultation as $consul) { ?>
+                    <?php 
+                        foreach($consultation as $consul) { 
+                            $report = str_replace('[', '', $consul->report);
+                            $report_akhir = str_replace(']', '', $report);
+                            $hasil_report = explode(',', $report_akhir);
+                    ?>
                     <div class="col-xl-6">
                         <div class="card">
                             <div class="card-header d-block">
-                                <h4 class="card-title">Default Accordion</h4>
-                                <p class="m-0 subtitle">Default accordion. Add <code>accordion</code> class in root</p>
+                                <h4 class="card-title"><?=date('D M Y', strtotime($consul->datetime))?></h4>
                             </div>
                             <div class="card-body">
                                 <!-- Default accordion -->
                                 <div id="accordion-one" class="accordion accordion-primary">
                                     <div class="accordion__item">
                                         <div class="accordion__header rounded-lg" data-toggle="collapse" data-target="#default_collapseOne">
-                                            <span class="accordion__header--text">Accordion Header One</span>
+                                            <span class="accordion__header--text">Hasil Konsultasi</span>
                                             <span class="accordion__header--indicator"></span>
                                         </div>
+                                        <?php 
+                                            foreach($hasil_report as $hr) {                                            
+                                        ?>
                                         <div id="default_collapseOne" class="collapse accordion__body show" data-parent="#accordion-one">
                                             <div class="accordion__body--text">
-                                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+                                                <?=$hr?>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="accordion__item">
-                                        <div class="accordion__header collapsed rounded-lg" data-toggle="collapse" data-target="#default_collapseTwo">
-                                            <span class="accordion__header--text">Accordion Header Two</span>
-                                            <span class="accordion__header--indicator"></span>
-                                        </div>
-                                        <div id="default_collapseTwo" class="collapse accordion__body" data-parent="#accordion-one">
-                                            <div class="accordion__body--text">
-                                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="accordion__item">
-                                        <div class="accordion__header collapsed rounded-lg" data-toggle="collapse" data-target="#default_collapseThree">
-                                            <span class="accordion__header--text">Accordion Header Three</span>
-                                            <span class="accordion__header--indicator"></span>
-                                        </div>
-                                        <div id="default_collapseThree" class="collapse accordion__body" data-parent="#accordion-one">
-                                            <div class="accordion__body--text">
-                                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
-                                            </div>
-                                        </div>
-                                    </div>
+                                        <?php } ?>
+                                    </div>                                    
                                 </div>
                             </div>
                         </div>
