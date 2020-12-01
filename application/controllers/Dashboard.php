7e9795ad60cf57d29510134b71383561b9062caf
fajar <fajarpratamap@gmail.com>
2020-12-02 06:26:22 +0800

    count klik

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -18,11 +18,15 @@ class Dashboard extends CI_Controller {
 
 	public function index()
 	{
+		$date = date('Y-m-d');
 		$data['graph'] = $this->db->query('select date, (select (select COUNT(mood)) as a from `mood_record` where mood = 1 group by mood) as m from `mood_record` group by date order by date desc limit 7')->result();
 		$data['mood'] = $this->m_mood->get_all();
 		$data['count'] = $this->db->query('select count(*) from mood_record')->row();
 		$data['last_karyawan'] = $this->m_auth->get_log();
 		$data['last_psikolog'] = $this->m_auth->get_log_psikolog();
+		$data['count_download'] = $this->db->query("SELECT value FROM analytic WHERE name = 'klik'")->row();
+		$data['count_log_psikolog'] = $this->db->query("SELECT COUNT(*) as count FROM log_login WHERE datetime like '%$date%' and status = 'Psikolog'")->row();
+		$data['count_log_Karyawan'] = $this->db->query("SELECT COUNT(*) as count FROM log_login WHERE datetime like '%$date%' and status = 'Karyawan'")->row();
 		$this->template->view('dashboard/index', $data);
 	}
 
--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -3,7 +3,7 @@
             <!-- row -->
 			<div class="container-fluid">
 				<div class="row">   
-                	<div class="col-xl-12">
+          <div class="col-xl-12">
 						<div class="card">
 							<div class="card-body">
 								<p class="font-w100 fs-20 text-black">Mood Grafik</p>
@@ -16,60 +16,96 @@
 						</div>
 
 					</div>              
-					<div class="col-xl-6 col-xxxl-12 col-lg-6">
+					<div class="col-xl-4 col-xxxl-12 col-lg-4">
 						<div class="card">
-                            <div class="card-header border-0 pb-0">
-                                <h4 class="card-title">Last Login Employee</h4>
-                            </div>
-                            <div class="card-body">
-                                <div id="DZ_W_TimeLine" class="widget-timeline dz-scroll" style="height:370px;">
-                                    <ul class="timeline">
-                                        <?php 
-                                          foreach ($last_karyawan as $karyawan) {										
-                                        ?>
-                                        <li>
-                                            <div class="timeline-badge primary"></div>
-                                            <a class="timeline-panel text-muted" href="#">
-                                                <span><?=date('D M Y', strtotime($karyawan->datetime))?></span>
-                                                <h6 class="mb-0"><?=$karyawan->username?> telah login.</h6>
-                                            </a>
-                                        </li>
-								                      	<?php } ?>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-					<div class="col-xl-6 col-xxxl-12 col-lg-6">
-					<div class="card">
-                            <div class="card-header border-0 pb-0">
-                                <h4 class="card-title">Last Login Psikolog</h4>
-                            </div>
-                            <div class="card-body">
-                                <div id="DZ_W_TimeLine1" class="widget-timeline dz-scroll style-1" style="height:370px;">
-                                    <ul class="timeline">
+              <div class="card-header border-0 pb-0">
+                <h4 class="card-title">Last Login Employee</h4>
+              </div>
+              <div class="card-body">
+                <div id="DZ_W_TimeLine" class="widget-timeline dz-scroll" style="height:370px;">
+                  <ul class="timeline">
+                    <?php 
+                      foreach ($last_karyawan as $karyawan) {										
+                    ?>
+                    <li>
+                      <div class="timeline-badge primary"></div>
+                        <a class="timeline-panel text-muted" href="#">
+                          <span><?=date('D M Y', strtotime($karyawan->datetime))?></span>
+                          <h6 class="mb-0"><?=$karyawan->username?> telah login.</h6>
+                        </a>
+                    </li>
+								    <?php } ?>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+					<div class="col-xl-4 col-xxxl-12 col-lg-4">
+					  <div class="card">
+              <div class="card-header border-0 pb-0">
+                <h4 class="card-title">Last Login Psikolog</h4>
+              </div>
+              <div class="card-body">
+                <div id="DZ_W_TimeLine1" class="widget-timeline dz-scroll style-1" style="height:370px;">
+                  <ul class="timeline">
 									<?php 
 										foreach ($last_psikolog as $psikolog) {										
 									?>
-                                        <li>
-                                            <div class="timeline-badge primary"></div>
-                                            <a class="timeline-panel text-muted" href="#">
-                                                <span><?=date('D M Y', strtotime($psikolog->datetime))?></span>
-                                                <h6 class="mb-0"><?=$psikolog->username?> telah login.</h6>
-                                            </a>
-                                        </li>
+                    <li>
+                      <div class="timeline-badge primary"></div>
+                        <a class="timeline-panel text-muted" href="#">
+                          <span><?=date('D M Y', strtotime($psikolog->datetime))?></span>
+                          <h6 class="mb-0"><?=$psikolog->username?> telah login.</h6>
+                        </a>
+                    </li>
 									<?php } ?>   
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    
-				</div>
+                  </ul>
+                </div>
+              </div>
             </div>
-        </div>
+          </div>
+          <div class="ccol-xl-3 col-lg-4 col-sm-12">
+            <div class="widget-stat card bg-primary">
+							<div class="card-body  p-4">
+								<div class="media">
+									<span class="mr-3">
+										<i class="la la-file-zip-o"></i>
+									</span>
+									<div class="media-body text-white">
+										<p class="mb-1">Total Downloads</p>
+										<h3 class="text-white"><?=$count_download->value;?></h3>
+									</div>
+								</div>
+							</div>
+              <div class="card-body  p-4">
+								<div class="media">
+									<span class="mr-3">
+										<i class="la la-user"></i>
+									</span>
+									<div class="media-body text-white">
+										<p class="mb-1">Total Login Psikolog Hari ini</p>
+										<h3 class="text-white"><?=$count_log_psikolog->count;?></h3>
+									</div>
+								</div>
+							</div>
+              <div class="card-body  p-4">
+								<div class="media">
+									<span class="mr-3">
+										<i class="la la-users"></i>
+									</span>
+									<div class="media-body text-white">
+										<p class="mb-1">Total Login Karyawan Hari ini</p>
+										<h3 class="text-white"><?=$count_log_Karyawan->count;?></h3>
+									</div>
+								</div>
+							</div>
+						</div>
+          </div>           
+				</div>
+      </div>
+    </div>
     
-		<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+	<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
   <script type="text/javascript">
     var ctx = document.getElementById('moodGrafik').getContext('2d');
     var chart = new Chart(ctx, {
