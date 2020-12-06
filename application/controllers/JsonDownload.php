9610d3204b9494d4ede5e19242c07791acb1059e
fajar <fajarpratamap@gmail.com>
2020-12-07 05:45:43 +0800

    notif group

--- application/controllers/JsonDownload.php
+++ application/controllers/JsonDownload.php
@@ -14,7 +14,18 @@ class JsonDownload extends CI_Controller {
 
 	public function getApp()
 	{
-		$data = $this->db->query("UPDATE analytic SET value=value+1 WHERE name = 'klik'");
+		$date = date('Y-m-d');
+		$select = $this->db->query("select * from analytic where datetime like '$date%'");
+		$data = array(
+			'name' 		=> 'klik',
+			'value' 	=> 1,
+			'datetime' 	=> date('Y-m-d H:i:s'),
+		);
+		if($select->num_rows() > 0){
+			$insert = $this->db->query("UPDATE analytic SET value=value+1 WHERE datetime like '$date%' and  name = 'klik'");
+		}else {
+			$insert = $this->db->insert('analytic', $data);
+		}
 		
 		redirect("https://drive.google.com/file/d/1_iStZ085zEB_LPgav3bgHc3ZITX2fdJr/view?usp=sharing");
 	}
--- application/views/analytics/index.php
+++ application/views/analytics/index.php
@@ -3,111 +3,45 @@
             <!-- row -->
 			<div class="container-fluid">
 				<div class="row">   
-          <div class="col-xl-12">
+          <div class="col-xl-4 col-xxxl-12 col-lg-4">
+						<div class="card">
+              <div class="card-header border-0 pb-0">
+                <h4 class="card-title">Filter Grafik</h4>
+              </div>
+              <div class="card-body">
+                <div class="basic-form">
+                  <form action="<?=base_url('analytics/index');?>" enctype="multipart/form-data" method="post">
+                    <div class="form-group">
+                      <label for="">Date</label>
+                      <input type="date" name="date" class="form-control input-default " placeholder="date">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Search</button>
+                    <button type="submit" class="btn btn-primary">Reset</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-8">
 						<div class="card">
 							<div class="card-body">
-								<p class="font-w100 fs-20 text-black">Mood Grafik</p>
+								<p class="font-w100 fs-20 text-black">Download Grafik</p>
 								<div class="row mx-0">
 									<div class="col-sm-12 col-lg-12 px-0">
-										<canvas id="moodGrafik" height="150"></canvas>
+										<canvas id="downloadGrafik" height="150"></canvas>
 									</div>
 								</div>
 							</div>
 						</div>
 
 					</div>              
-					<div class="col-xl-4 col-xxxl-12 col-lg-4">
-						<div class="card">
-              <div class="card-header border-0 pb-0">
-                <h4 class="card-title">Last Login Employee</h4>
-              </div>
-              <div class="card-body">
-                <div id="DZ_W_TimeLine" class="widget-timeline dz-scroll" style="height:370px;">
-                  <ul class="timeline">
-                    <?php 
-                      foreach ($last_karyawan as $karyawan) {										
-                    ?>
-                    <li>
-                      <div class="timeline-badge primary"></div>
-                        <a class="timeline-panel text-muted" href="#">
-                          <span><?=date('D M Y', strtotime($karyawan->datetime))?></span>
-                          <h6 class="mb-0"><?=$karyawan->username?> telah login.</h6>
-                        </a>
-                    </li>
-								    <?php } ?>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-					<div class="col-xl-4 col-xxxl-12 col-lg-4">
-					  <div class="card">
-              <div class="card-header border-0 pb-0">
-                <h4 class="card-title">Last Login Psikolog</h4>
-              </div>
-              <div class="card-body">
-                <div id="DZ_W_TimeLine1" class="widget-timeline dz-scroll style-1" style="height:370px;">
-                  <ul class="timeline">
-									<?php 
-										foreach ($last_psikolog as $psikolog) {										
-									?>
-                    <li>
-                      <div class="timeline-badge primary"></div>
-                        <a class="timeline-panel text-muted" href="#">
-                          <span><?=date('D M Y', strtotime($psikolog->datetime))?></span>
-                          <h6 class="mb-0"><?=$psikolog->username?> telah login.</h6>
-                        </a>
-                    </li>
-									<?php } ?>   
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="ccol-xl-3 col-lg-4 col-sm-12">
-            <div class="widget-stat card bg-primary">
-							<div class="card-body  p-4">
-								<div class="media">
-									<span class="mr-3">
-										<i class="la la-file-zip-o"></i>
-									</span>
-									<div class="media-body text-white">
-										<p class="mb-1">Total Downloads</p>
-										<h3 class="text-white"><?=$count_download->value;?></h3>
-									</div>
-								</div>
-							</div>
-              <div class="card-body  p-4">
-								<div class="media">
-									<span class="mr-3">
-										<i class="la la-user"></i>
-									</span>
-									<div class="media-body text-white">
-										<p class="mb-1">Total Login Psikolog Hari ini</p>
-										<h3 class="text-white"><?=$count_log_psikolog->count;?></h3>
-									</div>
-								</div>
-							</div>
-              <div class="card-body  p-4">
-								<div class="media">
-									<span class="mr-3">
-										<i class="la la-users"></i>
-									</span>
-									<div class="media-body text-white">
-										<p class="mb-1">Total Login Karyawan Hari ini</p>
-										<h3 class="text-white"><?=$count_log_Karyawan->count;?></h3>
-									</div>
-								</div>
-							</div>
-						</div>
-          </div>           
 				</div>
       </div>
     </div>
     
 	<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
   <script type="text/javascript">
-    var ctx = document.getElementById('moodGrafik').getContext('2d');
+    var ctx = document.getElementById('downloadGrafik').getContext('2d');
     var chart = new Chart(ctx, {
     type: 'bar',
     data: {
@@ -115,69 +49,25 @@
           <?php
             if (count($graph)>0) {
               foreach ($graph as $data) {
-                echo "'" .$data->date ."',";
+                echo "'" .date('d-m-Y', strtotime($data->datetime))."',";
               }
             }
           ?>
         ],
         datasets: [{
-            label: 'Tertekan',
+            label: 'Downloads',
             backgroundColor: '#FF6347',
             borderColor: '##93C3D2',
             data: [
               <?php
                 if (count($graph)>0) {
                    foreach ($graph as $data) {
-					$mood_count = $this->db->query("select COUNT(mood) as a from `mood_record` where mood = 1 and date = '$data->date' group by mood ;")->row();
-					if(!empty($mood_count->a)) {
-						echo $mood_count->a . ", ";
-					} else {
-						echo 0 , ", ";
-					}
-					
-                  }
-                }
-              ?>
-            ]
-        },{
-            label: 'Nyaman',
-            backgroundColor: '#ADD8E6',
-            borderColor: '##93C3D2',
-            data: [
-              <?php
-                if (count($graph)>0) {
-                   foreach ($graph as $data) {
-					$mood_count = $this->db->query("select COUNT(mood) as a from `mood_record` where mood = 2 and date = '$data->date' group by mood ;")->row();
-					if(!empty($mood_count->a)) {
-						echo $mood_count->a . ", ";
-					} else {
-						echo 0 , ", ";
-					}
-					
-                  }
-                }
-              ?>
-            ]
-        },{
-            label: 'Semangat',
-            backgroundColor: '#FF8C00',
-            borderColor: '##93C3D2',
-            data: [
-              <?php
-                if (count($graph)>0) {
-                   foreach ($graph as $data) {
-					$mood_count = $this->db->query("select COUNT(mood) as a from `mood_record` where mood = 3 and date = '$data->date' group by mood ;")->row();
-					if(!empty($mood_count->a)) {
-						echo $mood_count->a . ", ";
-					} else {
-						echo 0 , ", ";
-					}
-					
+                    echo "'" .($data->value)."',";
                   }
                 }
               ?>
             ]
         }]
-    },
-});
+      },
+    });
 </script>
